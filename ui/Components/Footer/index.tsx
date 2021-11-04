4fd0c41454a99e57ca74c3c2a0fe8598694a1c38
Alejo Taliento <alee.taliento@gmail.com>
2021-11-03 22:50:47 -0300

    feat: add Footer

--- ui/Components/Footer/index.tsx
+++ ui/Components/Footer/index.tsx
@@ -1,15 +1,54 @@
 import React from "react";
-import { Flex, Stack, Text } from "@chakra-ui/react";
-import Link from "next/link";
+import { Flex, Stack, Container, Text, Divider } from "@chakra-ui/react";
 
 import styles from "./index.module.scss";
 
-export interface FooterProps {}
+type Section = {
+  title: string;
+  description: string;
+};
+export interface FooterProps {
+  sections: [Section, Section?, Section?, Section?];
+}
 
-export const Footer: React.FC<FooterProps> = ({}) => {
+export const Footer: React.FC<FooterProps> = ({
+  sections = [
+    {
+      title: "Cristhal productos naturales",
+      description:
+        "Gracias por visitar nuestra tienda. Aqui podras en contrar alimentos saludables para Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla necessitatibus veritatis consequuntur, cupiditate qui repudiandae obcaecati incidunt.",
+    },
+    {
+      title: "Cristhal productos naturales",
+      description:
+        "Gracias por visitar nuestra tienda. Aqui podras en contrar alimentos saludables para Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla necessitatibus veritatis consequuntur, cupiditate qui repudiandae obcaecati incidunt.",
+    },
+    {
+      title: "Cristhal productos naturales",
+      description:
+        "Gracias por visitar nuestra tienda. Aqui podras en contrar alimentos saludables para Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla necessitatibus veritatis consequuntur, cupiditate qui repudiandae obcaecati incidunt.",
+    },
+    {
+      title: "Cristhal productos naturales",
+      description:
+        "Gracias por visitar nuestra tienda. Aqui podras en contrar alimentos saludables para Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla necessitatibus veritatis consequuntur, cupiditate qui repudiandae obcaecati incidunt.",
+    },
+  ],
+}) => {
   return (
     <Flex className={styles.root}>
-      <Stack direction={["column", "row"]} spacing="24px" />
+      <Stack direction={["column", "row"]} spacing="24px">
+        {sections.length &&
+          sections.map((section, index) => (
+            <>
+              <Container>
+                <Text fontSize="xl">{section.title}</Text>
+                <Text fontSize="sm">{section.description}</Text>
+              </Container>
+              {index < 3 ? <Divider orientation="vertical" /> : null}
+            </>
+          ))}
+      </Stack>
     </Flex>
   );
 };
